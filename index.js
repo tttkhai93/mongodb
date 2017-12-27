78dab8483f84349108486ee26fac12a931bf8861
Khai <Khai@tttkhai93@gmail.com>
2017-12-26 18:10:46 -0800

    fdsf

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const {mongooseURI} = require('./config/keys');
+const keys = require('./config/keys');
+//const {mongooseURI} = require('./config/keys');
 const app = express();
 
 require('./services/passport');
 require('./models/user');
 require('./routes/authRoutes')(app);
-mongoose.connect(mongooseURI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(keys.mongooseURI || 'mongodb://localhost:27017/TodoApp');
 
 const port = process.env.PORT || 3000;
 
